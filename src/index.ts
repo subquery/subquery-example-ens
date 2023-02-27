fdfdbe8c3fa912c33e6acd9d775a91b20aff1f3a
JQQQ <jiqiang90@hotmail.com>
2023-02-28 11:30:18 +1300

    add abab

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 //Exports all handler functions
+
+import {atob} from 'abab';
+global.atob = atob;
+
 export * from "./mappings/ensRegistry";
 export * from "./mappings/ethRegistrar";
 export * from "./mappings/nameWrapper";
