d60193617d9dbd7b4fcbca06a0cf288f10f19e4b
James Bayly <bayly.james@gmail.com>
2023-08-31 14:18:05 +0800

    project

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-//Exports all handler functions
-
-import { atob } from "abab";
-global.atob = atob;
-
-export * from "./mappings/ensRegistry";
-export * from "./mappings/ethRegistrar";
-export * from "./mappings/nameWrapper";
-export * from "./mappings/resolver";
